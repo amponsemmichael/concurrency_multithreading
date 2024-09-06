ab573360b3806de5a9329324d5e104ca808faabc
amponsemmichael <amponsem_michael@yahoo.com>
2024-09-06 13:47:20 +0000

    Feature(DeadLockScenerios): create codes that leads to deadlocks

--- src/deadlockscenerios/DeadLock.java
+++ src/deadlockscenerios/DeadLock.java
@@ -1,2 +1,36 @@
-package deadlockscenerios;public class DeadLock {
+package deadlockscenerios;
+
+public class DeadLock {
+    private final Object lock1 = new Object();
+    private final Object lock2 = new Object();
+
+    public void method1() {
+        synchronized (lock1) {
+            System.out.println("Lock1 acquired by method1");
+            try { Thread.sleep(100); } catch (InterruptedException e) {}
+            synchronized (lock2) {
+                System.out.println("Lock2 acquired by method1");
+            }
+        }
+    }
+
+    public void method2() {
+        synchronized (lock2) {
+            System.out.println("Lock2 acquired by method2");
+            try { Thread.sleep(100); } catch (InterruptedException e) {}
+            synchronized (lock1) {
+                System.out.println("Lock1 acquired by method2");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        DeadLock example = new DeadLock();
+        Thread t1 = new Thread(example::method1);
+        Thread t2 = new Thread(example::method2);
+
+        t1.start();
+        t2.start();
+    }
 }
+
