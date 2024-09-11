63ca5defb2dc4626cb43af6780dd8efdcd3de48e
amponsemmichael <amponsem_michael@yahoo.com>
2024-09-11 04:06:02 +0000

    modified Task file

--- src/ConcurrencyExample.java
+++ src/ConcurrencyExample.java
@@ -1,11 +1,10 @@
-//// Concurrency example using Runnable
-//
-//public class ConcurrencyExample {
-//    public static void main(String[] args) {
-//        Thread thread1 = new Thread(new Task("Task 1"));
-//        Thread thread2 = new Thread(new Task("Task 2"));
-//
-//        thread1.start();
-//        thread2.start();
-////    }
-//}
+// Concurrency example using Runnable
+public class ConcurrencyExample {
+    public static void main(String[] args) {
+        Thread thread1 = new Thread(new Task("Task 1"));
+        Thread thread2 = new Thread(new Task("Task 2"));
+
+        thread1.start();
+        thread2.start();
+}
+}
--- src/Task.java
+++ src/Task.java
@@ -1,14 +1,14 @@
-//class Task implements Runnable {
-//    private final String name;
-//
-//    public Task(String name) {
-//        this.name = name;
-//    }
-//
-//    @Override
-//    public void run() {
-//        for (int i = 0; i < 5; i++) {
-//            System.out.println(name + " - " + i);
-//        }
-//    }
-//}
+class Task implements Runnable {
+    private final String name;
+
+    public Task(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 5; i++) {
+            System.out.println(name + " - " + i);
+        }
+    }
+}
